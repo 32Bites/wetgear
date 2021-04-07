14899c519a3fba04f19070b415f6c1216219b459
Noah Shanaberger <noah@thenoah.party>
2021-04-07 14:43:57 -0400

    Add Argument

---
+++ argument.go
@@ -0,0 +1,81 @@
+package wetgear
+
+import (
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var ArgumentRegex = regexp.MustCompile(`[^" \n\r\t]+|"[^"\n\r\t]*"`)
+
+type Argument struct {
+	raw    string
+	quoted bool
+}
+
+func (a Argument) Empty() bool {
+	return a.raw == ""
+}
+
+func (a Argument) Raw() string {
+	return a.raw
+}
+
+func (a Argument) Quoted() bool {
+	return a.quoted
+}
+
+// ParseArguments takes in an input string and parses out valid arguments for use in commands
+func ParseArguments(content string) []Argument {
+	if len(content) == 0 {
+		return nil
+	}
+
+	arguments := make([]Argument, 0)
+	for _, match := range ArgumentRegex.FindAllString(content, -1) {
+		quoted := strings.HasPrefix(match, `"`) && strings.HasSuffix(match, `"`)
+		if quoted {
+			runes := []rune(match)
+			match = string(runes[1 : len(runes)-1])
+		}
+		arguments = append(arguments, Argument{match, quoted})
+	}
+
+	return arguments
+}
+
+// GetInt attempts to parse the argument as a int64. Upon failure, returns nil
+func (a Argument) GetInt() *int64 {
+	if i, err := strconv.ParseInt(a.raw, 10, 64); err == nil {
+		return &i
+	} else {
+		return nil
+	}
+}
+
+// GetFloat attempts to parse the argument as a float64. Upon failure, returns nil
+func (a Argument) GetFloat() *float64 {
+	if i, err := strconv.ParseFloat(a.raw, 64); err == nil {
+		return &i
+	} else {
+		return nil
+	}
+}
+
+// GetUint attempts to parse the argument as a uint64. Upon failure, returns nil
+func (a Argument) GetUint() *uint64 {
+	if i, err := strconv.ParseUint(a.raw, 10, 64); err == nil {
+		return &i
+	} else {
+		return nil
+	}
+}
+
+// GetBool attempts to parse the argument as a bool. Upon failure, returns nil
+func (a Argument) GetBool() *bool {
+	if i, err := strconv.ParseBool(a.raw); err == nil {
+		return &i
+	} else {
+		return nil
+	}
+}
